f897b74aa177f102bb35e4e2e5d3134d76a4ae89
Jeremie <jerpelletier13@icloud.com>
2026-01-27 16:48:12 -0500

    Ajout des tests unitaires pour les lois géométriques et poisson

--- test_unitaire.py
+++ test_unitaire.py
@@ -7,9 +7,12 @@ assert codeSource.inferieurOuEgal(codeSource.binomiale(10, 0.3, 4), 4) == 0.8497
 assert codeSource.superieur(codeSource.binomiale(10, 0.3, 4), 4) == 0.1507, "Test unitaire '>' échoué(binomiale)"
 assert codeSource.superieurOuEgal(codeSource.binomiale(10, 0.3, 4), 4) == 0.3504, "Test unitaire '>=' échoué(binomiale)"
 
+#Test unitaire pour la loi géométrique
 assert codeSource.geometrique(0.2, 2) == 0.16, "Test unitaire égalité/géométrique échoué"
 
+#Test unitaiare pour la loi de poisson
 assert codeSource.poisson(3, 2) == 0.2240, "Test unitaire égalité/poisson échoué"
+
 
 assert codeSource.binomiale(10, 0.5, 5) == 0.2051, "Erreurrrrrrrrrrrrrrrr"
 
