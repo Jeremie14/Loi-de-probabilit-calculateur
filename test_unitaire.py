a017a3756d79bab1e5544b6a248dad59d75c6302
Jeremie <jerpelletier13@icloud.com>
2026-01-28 15:34:39 -0500

    Ajout de la structure des autres tests unitaires

--- test_unitaire.py
+++ test_unitaire.py
@@ -9,11 +9,31 @@ assert codeSource.superieurOuEgal(codeSource.binomiale(10, 0.3, 4), 4) == 0.3504
 
 #Test unitaire pour la loi géométrique
 assert codeSource.geometrique(0.2, 2) == 0.16, "Test unitaire égalité/géométrique échoué"
+assert codeSource.inferieur(codeSource.geometrique(0.2, 3), 3) == 0.36, "Test unitaire '<' échoué(géométrique)"
+assert codeSource.inferieurOuEgal(codeSource.geometrique(0.2, 3), 3) == 0.488, "Test unitaire '<=' échoué(géométrique)"
+assert codeSource.superieur(codeSource.geometrique(0.2, 3), 3) == 0.512, "Test unitaire '>' échoué(géométrique)"
+assert codeSource.superieurOuEgal(codeSource.geometrique(0.2, 3), 3) == 0.64, "Test unitaire '>=' échoué(géométrique)"
 
 #Test unitaiare pour la loi de poisson
-assert codeSource.poisson(3, 2) == 0.2240, "Test unitaire égalité/poisson échoué"
+assert codeSource.poisson(2, 2) == 0.2707, "Test unitaire égalité/poisson échoué"
+assert codeSource.inferieur(codeSource.poisson(2, 2), 2) == 0.4060, "Test unitaire '<' échoué(géométrique)"
+assert codeSource.inferieurOuEgal(codeSource.poisson(2, 2), 2) == 0.6767, "Test unitaire '<=' échoué(géométrique)"
+assert codeSource.superieur(codeSource.poisson(2, 2), 2) == 0.5940, "Test unitaire '>' échoué(géométrique)"
+assert codeSource.superieurOuEgal(codeSource.poisson(2, 2), 2) == 0.3233, "Test unitaire '>=' échoué(géométrique)"
+
+#Test unitaire pour la loi exponentielle
+assert codeSource.inferieur(codeSource.exponentielle(), ) == , "Test unitaire '<' échoué(exponentielle)"
+assert codeSource.inferieurOuEgal(codeSource.exponentielle(), ) == , "Test unitaire '<=' échoué(exponentielle)"
+assert codeSource.superieur(codeSource.exponentielle(), ) == , "Test unitaire '>' échoué(exponentielle)"
+assert codeSource.superieurOuEgal(codeSource.exponentielle(), ) == , "Test unitaire '>=' échoué(exponentielle)"
 
 
-assert codeSource.binomiale(10, 0.5, 5) == 0.2051, "Erreurrrrrrrrrrrrrrrr"
+#Test unitaire pour la loi gamma
+assert codeSource.inferieur(codeSource.gamma(), ) == , "Test unitaire '<' échoué(gamma)"
+assert codeSource.inferieurOuEgal(codeSource.gamma(), ) == , "Test unitaire '<=' échoué(gamma)"
+assert codeSource.superieur(codeSource.gamma(), ) == , "Test unitaire '>' échoué(gamma)"
+assert codeSource.superieurOuEgal(codeSource.gamma(), ) == , "Test unitaire '>=' échoué(gamma)"
+
+
 
 
